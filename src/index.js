8c35c2e91a6f2ad5b583df5c2bad17b9ed8bde56
charlotte karimi <[charlotte.karimi@student.moringaschool.com]>
2022-12-16 15:23:30 +0300

    char

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("list").addEventListener("submit",(e)=>{
+    e.preventDefault()
+    tasklisting(e.target["new-task-description"].value)
+  })
 });
+
+function taskListing(text) {
+  let list =document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click',delFunc)
+  btn.textContent='X'
+  list.textContent=`${text}`
+  list.appendChild(btn)
+  document.querySelector("#tasks").appendChild(list)
+
+}
+function delFunc(e) {
+  e.target.parentnode.remove()
+}
